cdeabb90f971de12b0ffb71033b6101d9b499920
AadiXC0DE <aadityaz2077@gmail.com>
2023-08-14 22:42:08 +0530

    fixed homepage bug

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,21 +6,23 @@ import background from "../../public/background.png";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center mb-5">
-        <Image src={logo} width={156} height={156} className="mb-2"></Image>
-        <div className="flex items-center">
-          <h2 className="text-6xl font-semibold text-gray-900 text-center">
-            PROJECT SUBMIT
-          </h2>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full p-8 rounded-lg shadow-lg bg-white">
+        <div className="flex flex-col items-center justify-center mb-5">
+          <Image src={logo} width={156} height={156} className="mb-2" />
+          <div className="flex items-center">
+            <h2 className="text-4xl font-semibold text-gray-900 text-center">
+              PROJECT SUBMIT
+            </h2>
+          </div>
         </div>
-      </div>
 
-      <Link legacyBehavior href="/login">
-        <a className="block w-full bg-yellow-500 hover:bg-yellow-600 transition-colors duration-300 rounded-xl py-3 text-center text-white font-bold">
-          Submit Project
-        </a>
-      </Link>
+        <Link legacyBehavior href="/login">
+          <a className="block w-full bg-yellow-500 hover:bg-yellow-600 transition-colors duration-300 rounded-xl py-3 text-center text-white font-bold mt-4">
+            Submit Project
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
