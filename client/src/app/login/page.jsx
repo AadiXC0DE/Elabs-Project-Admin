5e506e72e2b064f0506ce32602131b0aa2da4986
AadiXC0DE <aadityaz2077@gmail.com>
2023-08-22 19:15:18 +0530

    made changes

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 import background from "../../../public/background.png";
 
 //login function
@@ -72,56 +73,64 @@ const LoginPage = () => {
     setPassword("");
   };
   //framer
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white rounded-xl p-10 shadow-lg max-w-xl">
-        <div className="flex flex-col items-center justify-center mb-5">
-          <div className="flex items-center mb-2">
-            <h1 className="text-6xl font-bold text-yellow-500 mr-2 ">ELABS</h1>
-            <h2 className="text-6xl font-semibold text-gray-900">PROJECT</h2>
+      <motion.div initial="hidden" animate="visible" variants={variants}>
+        <div className="bg-white rounded-xl p-10 shadow-lg max-w-xl">
+          <div className="flex flex-col items-center justify-center mb-5">
+            <div className="flex items-center mb-2">
+              <h1 className="text-6xl font-bold text-yellow-500 mr-2 ">
+                ELABS
+              </h1>
+              <h2 className="text-6xl font-semibold text-gray-900">PROJECT</h2>
+            </div>
           </div>
-        </div>
-        <p className="text-2xl text-gray-900 mb-5 text-center">
-          {isSignUp ? "Sign Up" : "Are you ready?"}
-        </p>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-5">
-            <input
-              type="text"
-              className="w-full py-2 px-4 rounded-md border focus:outline-none focus:border-yellow-500 text-black"
-              placeholder={"Roll No"}
-              value={name}
-              maxLength={9}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
+          <p className="text-2xl text-gray-900 mb-5 text-center">
+            {isSignUp ? "Sign Up" : "Are you ready?"}
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-5">
+              <input
+                type="text"
+                className="w-full py-2 px-4 rounded-md border focus:outline-none focus:border-yellow-500 text-black"
+                placeholder={"Roll No"}
+                value={name}
+                maxLength={9}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
 
-          <div className="mb-5">
-            <input
-              type="password"
-              className="w-full py-2 px-4 rounded-md border focus:outline-none focus:border-yellow-500 text-black"
-              placeholder="Password"
-              value={password}
-              maxLength={8}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-yellow-500 hover:bg-yellow-600 transition-colors duration-300 rounded-xl py-3 text-center text-white font-bold mb-2"
-          >
-            {isSignUp ? "Sign Up" : "Login"}
-          </button>
-          <button
-            type="button"
-            onClick={toggleSignUp}
-            className="w-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300 rounded-xl py-3 text-center text-gray-800 font-bold"
-          >
-            {isSignUp ? "Login Instead" : "Sign Up Instead"}
-          </button>
-        </form>
-      </div>
+            <div className="mb-5">
+              <input
+                type="password"
+                className="w-full py-2 px-4 rounded-md border focus:outline-none focus:border-yellow-500 text-black"
+                placeholder="Password"
+                value={password}
+                maxLength={8}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-yellow-500 hover:bg-yellow-600 transition-colors duration-300 rounded-xl py-3 text-center text-white font-bold mb-2"
+            >
+              {isSignUp ? "Sign Up" : "Login"}
+            </button>
+            <button
+              type="button"
+              onClick={toggleSignUp}
+              className="w-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300 rounded-xl py-3 text-center text-gray-800 font-bold"
+            >
+              {isSignUp ? "Login Instead" : "Sign Up Instead"}
+            </button>
+          </form>
+        </div>
+      </motion.div>
     </div>
   );
 };
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,27 +2,38 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
+import { motion } from "framer-motion";
 import background from "../../public/background.png";
+
+const variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+};
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex items-center justify-center ">
-      <div className="max-w-md w-full p-8 rounded-lg shadow-lg bg-white">
+    <div className="min-h-screen flex items-center justify-center">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        className="bg-white rounded-2xl p-10 shadow-lg max-w-xl px-48"
+      >
         <div className="flex flex-col items-center justify-center mb-5">
-          <Image src={logo} width={156} height={156} className="mb-2" />
+          <Image src={logo} width={156} height={156} className="mb-2"></Image>
           <div className="flex items-center">
-            <h2 className="text-4xl font-semibold text-gray-900 text-center">
+            <h2 className="text-6xl font-semibold text-gray-900 text-center">
               PROJECT SUBMIT
             </h2>
           </div>
         </div>
 
         <Link legacyBehavior href="/login">
-          <a className="block w-full bg-yellow-500 hover:bg-yellow-600 transition-colors duration-300 rounded-xl py-3 text-center text-white font-bold mt-4">
+          <a className="block w-full bg-yellow-500 hover:bg-yellow-600 transition-colors duration-300 rounded-xl py-3 text-center text-white font-bold">
             Submit Project
           </a>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 }
